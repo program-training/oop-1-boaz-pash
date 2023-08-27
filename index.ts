f77e25c0f2b0bba10048f9c714d2d3060966477e
Boaz Pash <boazpash123@gmail.com>
2023-08-27 11:02:11 +0300

    section 3 color rectangle

--- index.ts
+++ index.ts
@@ -1,31 +1,66 @@
-// את הקוד שלכם תכתבו כאן
-//  1. Rectangle
-class Rectangle {
+//  1. Rectangle;
+// class Rectangle {
+//   width: number;
+//   height: number;
+//   constructor(width: number, height: number) {
+//     this.width = width;
+//     this.height = height;
+//   }
+//   area(): number {
+//     return this.width * this.height;
+//   }
+// }
+// const rectangle = new Rectangle(2, 4);
+// console.log(rectangle.area());
+
+//  2. Square;
+// class Square extends Rectangle {
+//   long: number;
+//   constructor(long: number) {
+//     super(long, long);
+//     this.long = long;
+//   }
+// }
+// const square = new Square(4);
+// console.log(square.area());
+
+// 3. Shape;
+class Shape {
+  constructor() {}
+  info(): string {
+    return "This is a Shape";
+  }
+}
+
+class Rectangle extends Shape {
   width: number;
   height: number;
   constructor(width: number, height: number) {
+    super();
     this.width = width;
     this.height = height;
   }
   area(): number {
     return this.width * this.height;
   }
-}
-const rectangle = new Rectangle(2, 4);
-console.log(rectangle.area());
-
-//  2. Square - ריבוע
-class Square extends Rectangle {
-  long: number;
-  constructor(long: number) {
-    super(long, long);
-    this.long = long;
+  info(): string {
+    return "This is Rectangle";
   }
 }
-const square = new Square(4);
-console.log(square.area());
 
-// 3. Shape - צורה
+class ColoredRectangle extends Rectangle {
+  color: string;
+  constructor(width: number, height: number, color: string) {
+    super(width, height);
+    this.color = color;
+  }
+  info(): string {
+    return `This is a ${this.color} Rectangle`;
+  }
+}
+const rectangle = new ColoredRectangle(2, 4,"red");
+console.log(rectangle.area());
+console.log(rectangle.info());
 
 // 4. Method Chaining
 
